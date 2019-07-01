4acee864e38b19942c1f72f514cb88ed289b6548
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-01 12:08:02 +0200

    Starting State written

--- Text101/Assets/Scripts/AdventureGame.cs
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
+    [SerializeField] private Text textComponent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = "I'm a string";
     }
 
     // Update is called once per frame
